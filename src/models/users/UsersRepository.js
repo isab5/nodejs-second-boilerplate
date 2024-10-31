02b2448dd4f34fbe41f63676bf45195687442b9b
Isabella Borin <isabellaborin.tds1.senai@gmail.com>
2024-10-31 15:54:21 -0300

    feat: create post user method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -1,9 +1,19 @@
+import User from "./User.js";
+
 class UserRepository {
     constructor() {
         this.users = [];
     }
     getAllusers() {
         return this.users;
+    }
+
+    addUser(name, email, password) {
+        const newUser = new User(name, email, password);
+
+        this.users.push(newUser);
+
+        return newUser;
     }
 }
 
